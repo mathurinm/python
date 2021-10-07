aeb2b587ffddcddd4c6d1b6ab6367c71fb181bf9
mathurinm <mathurin.massias@gmail.com>
2021-10-07 11:03:32 +0200

    update project info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'python4ds'
+copyright = '2021, Mathurin Massias'
+author = 'Mathurin Massias'
 
 release = '0.1'
 version = '0.1.0'
